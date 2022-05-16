6af9aaac4153ebedd9cd18552766d8f2b2f5f1b8
Thomas <thomas.serdjebi@laplateforme.io>
2022-05-16 15:36:00 +0200

    projet terminé

--- element.php
+++ element.php
@@ -7,7 +7,7 @@ require('element_controller.php')
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <charset="ISO-8859-1">
-        <title>Détails</title>
+        <title>Spider Element</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
